7e4a5f2406c278a65338860aa6dc5328fcb58b5b
Alécio Dalprá <aleciodalpra@gmail.com>
2017-07-04 23:55:49 -0300

    alt_exclam

--- testeunitarioexec.py
+++ testeunitarioexec.py
@@ -4,7 +4,7 @@ from testeUnit import Testeunitario
 class MyTestCase(unittest.TestCase):
     def teste_unitario(self):
         testeunitario = Testeunitario()
-        self.assertEqual(testeunitario.message, 'Hello world!')
+        self.assertEqual(testeunitario.message, 'Hello world')
 
 if __name__ == '__main__':
     unittest.main()
